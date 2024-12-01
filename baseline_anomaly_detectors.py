4cd1b1c0c8dfd3c460681d8ee6bb35e7f23ca394
drg5567 <drg5567@rit.edu>
2024-12-01 11:53:28 -0500

    Implemented time series correlation algo

--- baseline_anomaly_detectors.py
+++ baseline_anomaly_detectors.py
@@ -17,7 +17,34 @@ def regression_based_analysis():
 
 
 def correlating_with_time_series(normal_set, abnormal_set):
-    return
+    combined_set = []
+    for i in range(len(normal_set)):
+        combined_set.append(("N", normal_set[i]))
+    for i in range(len(abnormal_set)):
+        combined_set.append(("A", abnormal_set[i]))
+
+    p = len(combined_set)
+    r = 3
+    total_sum = 0
+    for i in range(p):
+        for j in range(1, r + 1):
+            st = combined_set[i]
+            same_set = rth_nearest_neighbor(st, combined_set, j)
+            total_sum += same_set
+    return total_sum / (p * r)
+
+
+def rth_nearest_neighbor(item, combined_set, r):
+    item_class = item[0]
+    # Calculate 1D euclidean distances
+    distances = [(cls, abs(value - item)) for cls, value in combined_set]
+    sorted_distances = sorted(distances, key=lambda x: x[1])
+
+    class_str, distance = sorted_distances[r]  # would be r - 1 but item is in the set as well
+    if class_str == item_class:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
